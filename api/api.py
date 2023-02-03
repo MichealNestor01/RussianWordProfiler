5940fcfaceae543ba68e7aa8270aa47f8bb19f26
Micheal Nestor <michealnestor@outlook.com>
2023-02-03 09:10:25 +0000

    Attempted to add CORS to the api

--- api/api.py
+++ api/api.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
+from flask_cors import CORS
 from modules.profiler import ProfilerObj
 
 profiler = ProfilerObj()
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
