2c1019d3804f3ac62a5518971302de1fa856e4bf
Micheal Nestor <michealnestor@outlook.com>
2023-02-06 23:30:55 +0000

    Added the band selector component

--- frontend/russian-word-profiler/src/components/BandsSelector.js
+++ frontend/russian-word-profiler/src/components/BandsSelector.js
@@ -1,5 +1,48 @@
-const BandsSelector = () => {
-  return <h1>Bands Selector</h1>;
+import { Fragment, useState, useReducer } from "react";
+import { ChromePicker } from "react-color";
+import Band from "./Band";
+
+/*
+const colourBandReducer = (state, action) => {
+  switch (action.type) {
+    case "change_colour":
+      const newState = { ...state };
+      newState.map((band) => {
+        if (band.top === action.target) {
+          return { ...band, colour: action.colour };
+        }
+        return band;
+      });
+      return newState;
+    default:
+      return state;
+  }
+};
+*/
+
+const BandsSelector = ({ colourBands, dispatchColourBands }) => {
+  /*
+  const [colourBands, dispatchColourBands] = useReducer(colourBandReducer, [
+    { id: 0, top: 1000, colour: "#FF0000" },
+    { id: 1, top: 5000, colour: "#016B09" },
+    { id: 2, top: 6000, colour: "#0804D4" },
+    { id: 3, top: "rest", colour: "#F008D8" },
+  ]);
+  */
+  console.log(colourBands);
+  const bandDivs = colourBands.map((band) => {
+    return (
+      <Band
+        key={band.id}
+        id={band.id}
+        startTop={band.top}
+        startColour={band.colour}
+        change={dispatchColourBands}
+      />
+    );
+  });
+
+  return <section className="bands-selector">{bandDivs}</section>;
 };
 
 export default BandsSelector;
