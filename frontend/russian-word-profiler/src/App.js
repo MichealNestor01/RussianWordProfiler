8e114779e3a9a0997f1e08d85688220aa3d2e2f7
Micheal Nestor <michealnestor@outlook.com>
2023-04-01 10:35:10 +0100

    cleaned up

--- frontend/russian-word-profiler/src/App.js
+++ frontend/russian-word-profiler/src/App.js
@@ -1,27 +1,13 @@
-import { Fragment, useEffect, useState, useReducer } from "react";
+import { useState } from "react";
 import axios from "axios";
 import background from "./assets/background.png";
 import MainEditor from "./components/MainEditor";
-import FormattedOutput from "./components/FormattedOutput";
-import BandsSelector from "./components/BandsSelector";
 
-const apiURL = "russian-word-profiler-api.fseggvhtdefnbdez.uksouth.azurecontainer.io:5000";
-
-const band_color = {
-  0: "#eb4334",
-  1000: "#eb9934",
-  2000: "#ebe134",
-  3000: "#40eb34",
-};
+//const apiURL = "russian-word-profiler-api.fseggvhtdefnbdez.uksouth.azurecontainer.io:5000";
 
 function App() {
   const [input, setInput] = useState("");
-  //const [bands, setBands] = useState([]);
   const [wordData, setWordData] = useState([]);
-  const [wordBandPairs, setWordBandPairs] = useState({});
-  //const [textFormat, textFormatDispatch] = useReducer(textFormatReducer, {
-  //  lineBreaks: [],
-  //});
 
   const submitHandler = async () => {
     const response = await axios({
@@ -30,24 +16,12 @@ function App() {
       data: { text: input },
     });
     if (response.status === 200) {
-      //setBands(response.data);
       console.log(response.data);
       setWordData(response.data);
+    } else {
+      console.error(`ERROR CODE: ${response.status} - ${response.statusText}`);
     }
   };
-
-  /*
-  useEffect(() => {
-    let newWordBandPairs = {};
-    bands.forEach((band) => {
-      band.entries.forEach((entry) => {
-        newWordBandPairs[entry.lemma] = band.from;
-      });
-    });
-    setWordBandPairs(newWordBandPairs);
-    console.log(wordBandPairs);
-  }, [bands]);
-  */
 
   return (
     <div className="page-wrapper">
@@ -63,8 +37,9 @@ function App() {
           <div className="card"></div>
         </section>
       </section>
+      {/* This is a background splash image */}
       <div className="image-container">
-        <img src={background} className="image" />
+        <img src={background} alt="background splash" className="image" />
       </div>
     </div>
   );
