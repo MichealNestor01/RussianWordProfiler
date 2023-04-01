377712c0e83a06b4eba2ed55b97f55aec9632867
Micheal Nestor <michealnestor@outlook.com>
2023-04-01 11:03:37 +0100

    made the code neater

--- frontend/russian-word-profiler/src/components/BandsSelector.js
+++ frontend/russian-word-profiler/src/components/BandsSelector.js
@@ -1,33 +1,30 @@
-import { Fragment, useEffect } from "react";
+import { useEffect, useState } from "react";
 import BandConfig from "./BandConfig";
 import { useSelector } from "react-redux";
 
 const BandsSelector = () => {
+  const [bandDivs, setBandDivs] = useState([]);
   const bands = useSelector((state) => state.bands);
 
   const createBandDivs = () => {
     return bands.map((band, index) => {
       return (
-        <Fragment>
-          <div className={`bandContainer ${index > 0 && "withBorder"}`}>
-            <div className="bandColour" style={{ backgroundColor: band.colour }} />
-            {index > 0 ? (
-              <h5>
-                {parseInt(bands[index - 1].top) + 1}-{band.top}
-              </h5>
-            ) : (
-              <h5>Top {band.top}</h5>
-            )}
-          </div>
-        </Fragment>
+        <div className={`bandContainer ${index > 0 && "withBorder"}`} key={`band-${index}`}>
+          <div className="bandColour" style={{ backgroundColor: band.colour }} />
+          {index > 0 ? (
+            <h5>
+              {parseInt(bands[index - 1].top) + 1}-{band.top}
+            </h5>
+          ) : (
+            <h5>Top {band.top}</h5>
+          )}
+        </div>
       );
     });
   };
 
-  let bandDivs = createBandDivs();
-
   useEffect(() => {
-    bandDivs = createBandDivs();
+    setBandDivs(createBandDivs());
   }, [bands]);
 
   return (
