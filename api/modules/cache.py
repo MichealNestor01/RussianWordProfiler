0a3e6a8a672959302d9ceda88ce163ed3ec77a93
Micheal Nestor <michealnestor@outlook.com>
2024-06-06 19:38:58 +0100

    Created a word cache object to manage the cache instead of the profiler object

---
+++ api/modules/cache.py
@@ -0,0 +1,60 @@
+from typing import List, TypedDict, Dict, Any
+from datetime import datetime
+import pickle
+from .query_yandex import query_yandex_for_synonyms
+
+class WordDataCacheEntry(TypedDict):
+    synonyms: List[str]
+    last_updated: datetime
+    time_to_live: int
+
+class WordCache:
+    # Time to live levels in days
+    default_ttl = 30
+    
+    def __init__(self, file: str = None):
+        self.file = file
+        self.words: Dict[str, WordDataCacheEntry] = {}
+        self.loadFromFile(file)
+    
+    def loadFromFile(self, file: str) -> None:
+        try:
+            with open(file, "rb") as f:
+                self.words = pickle.load(f)
+        except FileNotFoundError:
+            self.words: Dict[str, WordDataCacheEntry] = {}
+
+    def save(self, file: str = None) -> None:
+        if file is None:
+            file = self.file
+        try:
+            with open(file, "wb") as f:
+                pickle.dump(self.words, f)
+        except:
+            print(f"CACHE ERROR: Failed to save cache: {file}")
+
+    def addWord(self, word: str, synonyms: List[str]) -> None:
+        self.words[word] = {
+            "synonyms": synonyms,
+            "last_updated": datetime.now(),
+            "time_to_live": self.default_ttl
+        }
+
+    async def get_synonyms(self, word: str) -> List[str]:
+        if word in self.words:
+            # if the word already exists in the cache, check if it is still valid
+            if (datetime.now() - self.words[word]["last_updated"]).days < self.words[word]["time_to_live"]:
+                return self.words[word]["synonyms"]
+            else:
+                # ttl has run out, so check if the data has chaned on yandex, if it has, resent ttl, else increase it 
+                # up to a maximum of 180 days
+                new_synonyms: Dict[str, Any] = await query_yandex_for_synonyms(word)
+                if new_synonyms != self.words[word]["synonyms"]:
+                    del self.words[word]
+                    self.addWord(word, new_synonyms)
+                else:
+                    self.words[word]["time_to_live"] += 30 if self.words[word]["time_to_live"] < 180 else 0
+        else: 
+            new_synonyms = await query_yandex_for_synonyms(word)
+            self.addWord(word, new_synonyms)
+        return self.words[word]["synonyms"]
