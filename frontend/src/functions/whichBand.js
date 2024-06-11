8bf58f78c78686c7efe2c624de21c1e0fa005f44
Ben Thirkill <ben.t.thirkill@gmail.com>
2024-06-11 19:53:58 +0100

    updated which band to check bottom

--- frontend/src/functions/whichBand.js
+++ frontend/src/functions/whichBand.js
@@ -1,7 +1,7 @@
 export function whichBand(rank, bands) {
     if (rank !== -1 && Object.keys(bands).length !== 0) {
         for (const [band, bandData] of Object.entries(bands)) {
-            if (rank <= bandData.topVal) {
+            if (rank <= bandData.topVal && rank >= bandData.bottomVal) {
                 return band;
             }
         }
