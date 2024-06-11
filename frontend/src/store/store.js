e720dc405380b71f76fb3202ad0912f246239cf3
Ben Thirkill <ben.t.thirkill@gmail.com>
2024-06-11 19:02:46 +0100

    Removed uneccessary slice

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,14 +3,13 @@ import textReducer from "./slices/textSlice";
 import frequencyBandsReducer from "./slices/frequencyBandsSlice";
 import siteStateReducer from "./slices/siteStateSlice";
 import statsReducer from "./slices/statsSlice";
-import selectedBandReducer from "./slices/selectedBandSlice";
+
 const store = configureStore({
   reducer: {
     bands: frequencyBandsReducer,
     siteState: siteStateReducer,
     text: textReducer,
     stats: statsReducer,
-    selectedBands: selectedBandReducer,
   },
 });
 
