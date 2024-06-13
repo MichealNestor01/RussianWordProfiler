726af33cb1d80dd54b79a829388934ec6ccc8ba1
Micheal Nestor <michealnestor@outlook.com>
2024-06-13 19:19:37 +0100

    made band config neater

--- frontend/src/components/dialogueBoxes/BandConfig/BandConfigPanel.js
+++ frontend/src/components/dialogueBoxes/BandConfig/BandConfigPanel.js
@@ -113,60 +113,56 @@ import {
 //   );
 // };
 
-const BandConfig = ({}) => {
+// Wrap the band config panel in a dialog box.
+// We add an onclose that saves the configuration when closing the dialog box.
+const BandConfigPanel = ({ active, onClose }) => {
   const [activeIndex, setActiveIndex] = useState(-1);
   const dispatch = useDispatch();
   const bands = useSelector((state) => state.bands);
 
   return (
-    <Fragment>
-      {Object.keys(bands).map((band) => {
-        return (
-          <Band
-            key={band}
-            id={band}
-            colour={bands[band].colour}
-            top={bands[band].topVal}
-            bottom={bands[band].bottomVal}
-          />
-        );
-      })}
-
-      <NewBand />
-
-      <div className="orDivider">
-        <div className="divider"></div>
-        <p>OR</p>
-        <div className="divider"></div>
-      </div>
-
-      <div className="bandConfigOptions">
-        <button>
-          <SwatchIcon className="configIcon" />
-          Use Ready-Made Presets
-        </button>
-        <button>
-          <CloudArrowDownIcon className="configIcon" />
-          Save current preset to local file
-        </button>
-        <button>
-          <CloudArrowUpIcon className="configIcon" />
-          Load a preset from a local file
-        </button>
-      </div>
-    </Fragment>
-  );
-};
-
-// Wrap the band config panel in a dialog box.
-// We add an onclose that saves the configuration when closing the dialog box.
-const BandConfigPanel = ({ active, onClose }) => {
-  return (
     <DialogBox
       header={<h1>Band Configuration</h1>}
-      content={<BandConfig />}
       active={active}
       onClose={onClose}
+      content={
+        <Fragment>
+          {Object.keys(bands).map((band) => {
+            return (
+              <Band
+                key={band}
+                id={band}
+                colour={bands[band].colour}
+                top={bands[band].topVal}
+                bottom={bands[band].bottomVal}
+              />
+            );
+          })}
+
+          <NewBand />
+
+          <div className="orDivider">
+            <div className="divider"></div>
+            <p>OR</p>
+            <div className="divider"></div>
+          </div>
+
+          <div className="bandConfigOptions">
+            <button>
+              <SwatchIcon className="configIcon" />
+              Use Ready-Made Presets
+            </button>
+            <button>
+              <CloudArrowDownIcon className="configIcon" />
+              Save current preset to local file
+            </button>
+            <button>
+              <CloudArrowUpIcon className="configIcon" />
+              Load a preset from a local file
+            </button>
+          </div>
+        </Fragment>
+      }
     />
   );
 };
