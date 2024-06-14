252c9ce6d33dca939d929c948a614d14758d2a74
Ben Thirkill <ben.t.thirkill@gmail.com>
2024-06-14 14:12:10 +0100

    Created Preset Selection Component

---
+++ frontend/src/components/dialogueBoxes/BandConfig/PresetSelection.js
@@ -0,0 +1,25 @@
+// import { PlusIcon } from "@heroicons/react/24/solid";
+
+const PresetSelection = () => {
+    return (
+        <div className="presetOptionsContainer">
+            <h2>Ready-Made Presets</h2>
+
+            <div className="presetContainer">
+                <div className="preset">preset</div>
+                <div className="preset">preset</div>
+            </div>
+
+            <h2>Presets uploaded from this device</h2>
+            <div className="presetContainer">
+                <div className="preset">preset</div>
+                <div className="preset">preset</div>
+                <div className="preset">preset</div>
+                <div className="preset">preset</div>
+                <div className="preset">preset</div>
+            </div>
+        </div>
+    );
+};
+
+export default PresetSelection;
