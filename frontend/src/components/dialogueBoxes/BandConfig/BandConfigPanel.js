7641836b6623fedea0505623eb8b3e730ce2288e
Micheal Nestor <michealnestor@outlook.com>
2024-06-18 12:30:28 +0100

    Close colour picker when prest panel is opened

--- frontend/src/components/dialogueBoxes/BandConfig/BandConfigPanel.js
+++ frontend/src/components/dialogueBoxes/BandConfig/BandConfigPanel.js
@@ -7,14 +7,14 @@ import PresetSelection from "./PresetSelection/PresetSelection";
 import DialogBox from "../DialogBox";
 import { useSelector, useDispatch } from "react-redux";
 import {
-    SwatchIcon,
-    CloudArrowDownIcon,
-    CloudArrowUpIcon,
+  SwatchIcon,
+  CloudArrowDownIcon,
+  CloudArrowUpIcon,
 } from "@heroicons/react/24/solid";
 import {
-    loadPreset,
-    saveBands,
-    addNewPreset,
+  loadPreset,
+  saveBands,
+  addNewPreset,
 } from "../../../store/slices/frequencyBandsSlice";
 import { downloadJSON } from "../../../functions/downloadJSON";
 import { getDatetimeString } from "../../../functions/getDatetimeString";
@@ -43,122 +43,114 @@ import { handleJsonUpload } from "../../../functions/uploadJSON";
  * )
  */
 const BandConfigPanel = ({ active, onClose }) => {
-    const dispatch = useDispatch();
-    const fileInputRef = useRef(null);
-    const { bands, presets } = useSelector((state) => state.bandsSlice);
-    const [activeIndex, setActiveIndex] = useState(-1);
-    const [showPresets, setShowPresets] = useState(false);
-    const [currentPreset, setCurrentPreset] = useState({});
+  const dispatch = useDispatch();
+  const fileInputRef = useRef(null);
+  const { bands, presets } = useSelector((state) => state.bandsSlice);
+  const [activeIndex, setActiveIndex] = useState(-1);
+  const [showPresets, setShowPresets] = useState(false);
+  const [currentPreset, setCurrentPreset] = useState({});
 
-    useEffect(() => {
-        const tempCurrentPreset = {
-            isDefault: false,
-            name: "Current Preset",
-            bands: Object.keys(bands).map((band) => ({
-                top: bands[band].topVal,
-                colour: bands[band].colour,
-            })),
-        };
-        setCurrentPreset(tempCurrentPreset);
-    }, [bands, setCurrentPreset]);
-
-    const downloadCurrentPreset = () => {
-        const now = getDatetimeString();
-        downloadJSON(
-            { ...currentPreset, name: `Preset-${now}` },
-            "Russian Word Profiler Preset Configuration File",
-            `RussianWordProfilerPreset-${now}.json`
-        );
+  useEffect(() => {
+    const tempCurrentPreset = {
+      isDefault: false,
+      name: "Current Preset",
+      bands: Object.keys(bands).map((band) => ({
+        top: bands[band].topVal,
+        colour: bands[band].colour,
+      })),
     };
+    setCurrentPreset(tempCurrentPreset);
+  }, [bands, setCurrentPreset]);
 
-    const updateCurrentPreset = (preset) => {
-        setCurrentPreset(preset);
-        console.log(preset);
-        dispatch(loadPreset(preset));
-    };
-
-    return (
-        <DialogBox
-            header={<h1>Band Configuration</h1>}
-            active={active}
-            onClose={() => {
-                onClose();
-                dispatch(saveBands());
-                setActiveIndex(-1);
-            }}
-            content={
-                <Fragment>
-                    <AnimatePresence initial={false}>
-                        {Object.keys(bands).map((band) => {
-                            return (
-                                <motion.div
-                                    {...scaleAnimation}
-                                    key={band + "-container"}
-                                >
-                                    <Band
-                                        key={band}
-                                        id={band}
-                                        colour={bands[band].colour}
-                                        top={bands[band].topVal}
-                                        bottom={bands[band].bottomVal}
-                                        setActiveIndex={setActiveIndex}
-                                        activeIndex={activeIndex}
-                                    />
-                                </motion.div>
-                            );
-                        })}
-                    </AnimatePresence>
-
-                    <NewBand />
-
-                    <div className="orDivider">
-                        <div className="divider"></div>
-                        <p>OR</p>
-                        <div className="divider"></div>
-                    </div>
-
-                    <div className="bandConfigOptions">
-                        <button onClick={() => setShowPresets(true)}>
-                            <SwatchIcon className="configIcon" />
-                            Use Ready-Made Presets
-                        </button>
-
-                        <button onClick={downloadCurrentPreset}>
-                            <CloudArrowDownIcon className="configIcon" />
-                            Save current preset to local file
-                        </button>
-                        <button onClick={() => fileInputRef.current.click()}>
-                            <CloudArrowUpIcon className="configIcon" />
-                            Load a preset from a local file
-                        </button>
-                        <input
-                            ref={fileInputRef}
-                            type="file"
-                            accept=".json"
-                            style={{ display: "none" }}
-                            onChange={(event) =>
-                                handleJsonUpload(
-                                    event,
-                                    validatePresetJson,
-                                    (json) => {
-                                        setCurrentPreset(json);
-                                        dispatch(addNewPreset(json));
-                                        dispatch(loadPreset(json.name));
-                                    }
-                                )
-                            }
-                        />
-                    </div>
-                    <PresetSelection
-                        active={showPresets}
-                        onClose={() => setShowPresets(false)}
-                        currentPreset={currentPreset}
-                        setCurrentPreset={setCurrentPreset}
-                    />
-                </Fragment>
-            }
-        />
+  const downloadCurrentPreset = () => {
+    const now = getDatetimeString();
+    downloadJSON(
+      { ...currentPreset, name: `Preset-${now}` },
+      "Russian Word Profiler Preset Configuration File",
+      `RussianWordProfilerPreset-${now}.json`
     );
+  };
+
+  return (
+    <DialogBox
+      header={<h1>Band Configuration</h1>}
+      active={active}
+      onClose={() => {
+        onClose();
+        dispatch(saveBands());
+        setActiveIndex(-1);
+      }}
+      content={
+        <Fragment>
+          <AnimatePresence initial={false}>
+            {Object.keys(bands).map((band) => {
+              return (
+                <motion.div {...scaleAnimation} key={band + "-container"}>
+                  <Band
+                    key={band}
+                    id={band}
+                    colour={bands[band].colour}
+                    top={bands[band].topVal}
+                    bottom={bands[band].bottomVal}
+                    setActiveIndex={setActiveIndex}
+                    activeIndex={activeIndex}
+                  />
+                </motion.div>
+              );
+            })}
+          </AnimatePresence>
+
+          <NewBand />
+
+          <div className="orDivider">
+            <div className="divider"></div>
+            <p>OR</p>
+            <div className="divider"></div>
+          </div>
+
+          <div className="bandConfigOptions">
+            <button
+              onClick={() => {
+                setShowPresets(true);
+                setActiveIndex(-1);
+              }}
+            >
+              <SwatchIcon className="configIcon" />
+              Use Ready-Made Presets
+            </button>
+
+            <button onClick={downloadCurrentPreset}>
+              <CloudArrowDownIcon className="configIcon" />
+              Save current preset to local file
+            </button>
+            <button onClick={() => fileInputRef.current.click()}>
+              <CloudArrowUpIcon className="configIcon" />
+              Load a preset from a local file
+            </button>
+            <input
+              ref={fileInputRef}
+              type="file"
+              accept=".json"
+              style={{ display: "none" }}
+              onChange={(event) =>
+                handleJsonUpload(event, validatePresetJson, (json) => {
+                  setCurrentPreset(json);
+                  dispatch(addNewPreset(json));
+                  dispatch(loadPreset(json.name));
+                })
+              }
+            />
+          </div>
+          <PresetSelection
+            active={showPresets}
+            onClose={() => setShowPresets(false)}
+            currentPreset={currentPreset}
+            setCurrentPreset={setCurrentPreset}
+          />
+        </Fragment>
+      }
+    />
+  );
 };
 
 export default BandConfigPanel;
