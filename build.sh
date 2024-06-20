4f55dccdd40d6735c8602277500ffff4b9cb1b2d
Micheal Nestor <michealnestor@outlook.com>
2024-06-20 21:04:42 +0100

    updated build script and gitignore

--- build.sh
+++ build.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
-cd frontend/russian-word-profiler/
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Navigate to the frontend directory and install dependencies
+cd frontend/
+npm install
+
+# run frontend tests here if and when made
+
+# Build the frontend
 npm run build
-cp build/index.html ../../api/templates/
-cp -R build/static/ ../../api/
+
+# Navigate to the API directory and activate the virtual environment
+cd ../api
+
+# Check if .venv directory exists, if not, create a new virtual environment
+if [ ! -d ".venv" ]; then
+    python3 -m venv .venv
+fi
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Run the backend tests
+python -m unittest discover -s tests
+
+# Now try to package the build
+cd ..
+SOURCE_DIR="api"
+ZIP_FILE="russian-word-profiler.zip"
+
+zip -r "$ZIP_FILE" "$SOURCE_DIR" -x "*$SOURCE_DIR/.venv/*" -x "*$SOURCE_DIR/sphinx-docs/*" -x "*$SOURCE_DIR/tests/*" -x "*$SOURCE_DIR/README.md" -x "*$SOURCE_DIR/word_data_cache.pickle*" -x "*$SOURCE_DIR/__pycache__/*" -x "*$SOURCE_DIR/.env"
