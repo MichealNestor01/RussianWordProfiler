bcc63d6bece3c0c0ac744584cd14d9c3c9fb0284
Micheal Nestor <michealnestor@outlook.com>
2024-06-21 18:53:35 +0100

    changed formatting

--- frontend/src/components/generic/InputWithValidation.js
+++ frontend/src/components/generic/InputWithValidation.js
@@ -1,44 +1,41 @@
 import DialogBox from "../dialogueBoxes/DialogBox";
 
 const InputWithValidation = ({
-    active,
-    onClose,
-    text,
-    updateText,
-    isValid,
-    placeholder,
-    title,
-    onCancel,
-    onSave,
+  active,
+  onClose,
+  text,
+  updateText,
+  isValid,
+  placeholder,
+  title,
+  onCancel,
+  onSave,
 }) => {
-    return (
-        <DialogBox
-            active={active}
-            onClose={onClose}
-            header={<h1>{title}</h1>}
-            content={
-                <div className="inputWithValidationContainer">
-                    <p>{placeholder}</p>
-                    <input
-                        text={text}
-                        onChange={(e) => updateText(e.target.value)}
-                        style={{
-                            color: isValid ? "black" : "red",
-                        }}
-                    />
-                    <div className="buttonContainer">
-                        <button onClick={onCancel}>Cancel</button>
-                        <button
-                            className={isValid ? "" : "invalid"}
-                            onClick={onSave}
-                        >
-                            Save
-                        </button>
-                    </div>
-                </div>
-            }
-        />
-    );
+  return (
+    <DialogBox
+      active={active}
+      onClose={onClose}
+      header={<h1>{title}</h1>}
+      content={
+        <div className="inputWithValidationContainer">
+          <p>{placeholder}</p>
+          <input
+            text={text}
+            onChange={(e) => updateText(e.target.value)}
+            style={{
+              color: isValid ? "black" : "red",
+            }}
+          />
+          <div className="buttonContainer">
+            <button onClick={onCancel}>Cancel</button>
+            <button className={isValid ? "" : "invalid"} onClick={onSave}>
+              Save
+            </button>
+          </div>
+        </div>
+      }
+    />
+  );
 };
 
 export default InputWithValidation;
